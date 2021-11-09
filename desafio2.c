af68ea0043f81e7e607002e69cdf0ac9396996b7
Henri Venturini <52206130+henriventurini@users.noreply.github.com>
2021-11-09 19:11:47 -0300

    [NOVO] Desafio adicionado
    Referente ao 'desafio2.c'

---
+++ desafio2.c
@@ -0,0 +1,10 @@
+/*
+* Simulador de ataque zumbi, cria uma mapa com zumbis e humanos e simula seu estado cada dia até que o total de dias seja o desejado.
+* Criado por: Henri Boraschi Venturini (RA 2760482121027) e Pedro Henrique Vieira (RA ?)
+* Data: 09-11-2021
+*/
+#include <stdio.h>
+
+int main(){
+	
+}
